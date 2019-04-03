6246c6c9542aac3a7d7523ca62c82e9e1e084180
大谷東彦 <haruhiko@weseek.co.jp>
2019-04-03 11:50:11 +0900

    :new: init typeORM - init typeORM - add user

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Connection } from 'typeorm';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot(),
+    UserModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
