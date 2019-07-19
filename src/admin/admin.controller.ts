6a0fc02d78098c151f8fec6029361c651f0ce87e
大谷東彦 <haruhiko@weseek.co.jp>
2019-07-19 23:37:52 +0900

    - add admin page   - add index page

--- src/admin/admin.controller.ts
+++ src/admin/admin.controller.ts
@@ -5,6 +5,8 @@ import { ComposersService } from 'src/playing-logs/composers/composers.service';
 import { CountriesService } from 'src/playing-logs/countries/countries.service';
 import { Country } from 'src/playing-logs/countries/countries.entity';
 import { Composer } from 'src/playing-logs/composers/composers.entity';
+import * as hbs from 'hbs';
+import { join } from 'path';
 
 @Controller('admin')
 export class AdminController {
@@ -12,28 +14,30 @@ export class AdminController {
     private readonly usersService: UsersService,
     private readonly composersService: ComposersService,
     private readonly countriesService: CountriesService,
-  ) {}
+  ) {
+    hbs.registerPartials(join(__dirname, '../..', 'views/admin/partials'));
+  }
   @Get()
   @Render('admin/index')
   root() {
-    return { message: 'Hello world!' };
+    return { title:'yarilog管理画面' };
   }
   @Get("countries")
   @Render('admin/countries')
   async countries() {
     const countries: Country[] = await this.countriesService.findAll();
-    return { countries: countries };
+    return { countries: countries, title: '国一覧' };
   }
   @Get("composers")
   @Render('admin/composers')
   async composers() {
     const composers: Composer[] = await this.composersService.findAll();
-    return { composers: composers };
+    return { composers: composers, title: '作曲家一覧'  };
   }
   @Get("users")
   @Render('admin/users')
   async users() {
     const users: User[] = await this.usersService.findAll();
-    return { users: users };
+    return { users: users, title: 'ユーザ一覧'  };
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { join } from 'path';
 import { AppModule } from './app.module';
+import * as hbs from 'hbs';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(
@@ -11,7 +12,8 @@ async function bootstrap() {
   app.useStaticAssets(join(__dirname, '..', 'public'));
   app.setBaseViewsDir(join(__dirname, '..', 'views'));
   app.setViewEngine('hbs');
-  
+  // コントローラの方でコントローラ毎に設定がしたい 今は admin しかないから困らない
+  app.set('view options', { layout: 'admin/layout' });
   app.enableCors({
     origin: '*',
     allowedHeaders: 'Origin, X-Requested-With, Content-Type, Accept, Authorization',
