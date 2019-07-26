364a7e8e146196927259f0bd21811eafad4b47b2
大谷東彦 <haruhiko@weseek.co.jp>
2019-07-26 22:11:02 +0900

    - typeorm のコンフィグを環境変数で読み込むように

---
+++ ormconfig.js
@@ -0,0 +1,12 @@
+module.exports = {
+  "type": "mysql",
+  "host": "db",
+  "port": 3306,
+  "username": process.env.DB_USERNAME,
+  "password": process.env.DB_PASSWORD,
+  "database": "yarilog",
+  "entities": [process.env.TYPEORM_ENTITIES],
+  "charset": "utf8mb4",
+  "synchronize": true,
+  // "logging": ["query", "error"]
+}
